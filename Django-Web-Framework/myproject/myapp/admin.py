2b4850df68b7f27062fde5be57c8234555e415d4
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-03-27 11:06:44 +0200

    Exercise: Models using Foreign Keys

--- Django-Web-Framework/myproject/myapp/admin.py
+++ Django-Web-Framework/myproject/myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Drinks
+from .models import DrinksCategory, Drinks
 
 # Register your models here.
+admin.site.register(DrinksCategory)
 admin.site.register(Drinks)
