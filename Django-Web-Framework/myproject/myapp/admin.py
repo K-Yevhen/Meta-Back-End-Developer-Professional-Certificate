72ddd93d37a1a8a5cebdda6de2dead3701e48c66
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-03-28 10:23:07 +0200

    exercise-using-django-admin

--- Django-Web-Framework/myproject/myapp/admin.py
+++ Django-Web-Framework/myproject/myapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 # from .models import DrinksCategory, Drinks
-from .models import Booking
+from .models import Booking, Employees
 
 # Register your models here.
 # admin.site.register(DrinksCategory)
 # admin.site.register(Drinks)
 admin.site.register(Booking)
+admin.site.register(Employees)
