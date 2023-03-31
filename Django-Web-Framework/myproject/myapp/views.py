2d9543cea47fe0e97e1c67db82d15cebdb6026ac
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-03-31 10:30:51 +0200

    Exercise: Creating Templates

--- Django-Web-Framework/myproject/myapp/views.py
+++ Django-Web-Framework/myproject/myapp/views.py
@@ -34,3 +34,11 @@ def drinks(request, drink_name):
     }
     choice_of_drink = drink[drink_name]
     return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
+
+def about(request):
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
+    return render(request, "about.html", {'content': about_content})
+
+def menu(request):
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
+    return render(request, "menu.html", {'content': about_content})
