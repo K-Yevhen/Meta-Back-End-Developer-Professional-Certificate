13fce02582023b4111b721f449135ce55185e3d6
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-03-31 11:42:22 +0200

    exercise-working-with-templates

--- Django-Web-Framework/myproject/myapp/views.py
+++ Django-Web-Framework/myproject/myapp/views.py
@@ -24,8 +24,8 @@ def home(request):
 # def menu(request):
 #     return HttpResponse("Menu for Little Lemon")
 
-def book(request):
-    return HttpResponse("Make a booking")
+# def book(request):
+#     return HttpResponse("Make a booking")
 
 def drinks(request, drink_name):
     drink = {
@@ -36,16 +36,28 @@ def drinks(request, drink_name):
     choice_of_drink = drink[drink_name]
     return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
 
-def about(request):
-    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
-    return render(request, "about.html", {'content': about_content})
+# def about(request):
+#     about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
+#     return render(request, "about.html", {'content': about_content})
 
 # def menu(request):
 #     about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
 #     return render(request, "menu.html", {'content': about_content})
 
 
+# def menu(request):
+#     menu_items = Menu.objects.all()
+#     items_dict = {'menu': menu_items}
+#     return render(request, 'menu.html', items_dict)
+
+def home(request):
+    return render(request, 'index.html')
+
 def menu(request):
-    menu_items = Menu.objects.all()
-    items_dict = {'menu': menu_items}
-    return render(request, 'menu.html', items_dict)
+    return render(request, 'menu.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    return render(request, 'book.html')
