7cf18d1768e9ade827ecf24c73b2d41101a108aa
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-06-12 12:05:59 +0200

    Exercise: User account management
    Goal
    In this exercise, you will use authentication and validation mechanisms inside DRF. You need to do this activity using VS code and Insomnia on your computer. If you haven't set up your development environment yet, revisit
    Installing VS code
    ,
    Setting up tools and environment
     and
    Create a Django Project using pipenv
    .
    
    Objectives
    By the end of this exercise, you will be able to:
    
    Add form validators to form data
    
    Perform token and session authentication while using a DRF form
    
    Use the Djoser and authtoken packages for default routes
    
    Use the Django admin panel for creating new users and tokens

--- APIs/LittleLemon/LittleLemonDRF/urls.py
+++ APIs/LittleLemon/LittleLemonDRF/urls.py
@@ -7,6 +7,7 @@ from . import views
 # ]
 
 urlpatterns = [
-    path('category', views.CategoriesView.as_view()),
-    path('menu-items', views.MenuItemsView.as_view()),
+    # path('category', views.CategoriesView.as_view()),
+    # path('menu-items', views.MenuItemsView.as_view()),
+    path('ratings', views.RatingsView.as_view()),
 ]
--- APIs/LittleLemon/LittleLemonDRF/views.py
+++ APIs/LittleLemon/LittleLemonDRF/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import MenuItem, Category
-from .serializers import MenuItemSerializer, CategorySerializer
+# from .models import MenuItem, Category
+# from .serializers import MenuItemSerializer, CategorySerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Rating
+from .serializers import RatingSerializer
 
 # Create your views here.
 
@@ -14,14 +17,24 @@ from rest_framework import generics
 #     serializer_class = MenuItemSerializers
 
 
-class CategoriesView(generics.ListCreateAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+# class CategoriesView(generics.ListCreateAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+#
+#
+# class MenuItemsView(generics.ListCreateAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+#     ordering_fields = ['price', 'inventory']
+#     filterset_fields = ['price', 'inventory']
+#     search_fields = ['title']
 
+class RatingsView(generics.ListCreateAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
 
-class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
-    ordering_fields = ['price', 'inventory']
-    filterset_fields = ['price', 'inventory']
-    search_fields = ['title']
+    def get_permissions(self):
+        if (self.request.method == 'GET'):
+            return []
+
+        return [IsAuthenticated()]
