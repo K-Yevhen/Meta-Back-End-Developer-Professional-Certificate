928fac9d79283c77c398f5a724fc8adcbe6d4292
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-06-13 12:20:49 +0200

    modified views

--- APIs/LittleLemon/LittleLemonDRF/models.py
+++ APIs/LittleLemon/LittleLemonDRF/models.py
@@ -30,12 +30,17 @@ class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True)
 
+    def __str__(self):
+        return self.title
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Cart(models.Model):
@@ -48,6 +53,9 @@ class Cart(models.Model):
     class Meta:
         unique_together = ['menuitem', 'user']
 
+    def __str__(self):
+        return self.menuitem.title
+
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -55,6 +63,9 @@ class Order(models.Model):
     status = models.BooleanField(db_index=True, default=0)
     total = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(db_index=True)
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.delivery_crew.username}"
 
 
 class OrderItem(models.Model):
@@ -66,3 +77,6 @@ class OrderItem(models.Model):
 
     class Meta:
         unique_together = ['order', 'menuitem']
+
+    def __str__(self):
+        return f"{self.order.username} -> {self.menuitem}"
--- APIs/LittleLemon/LittleLemonDRF/views.py
+++ APIs/LittleLemon/LittleLemonDRF/views.py
@@ -52,10 +52,13 @@ from rest_framework.response import Response
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from django.contrib.auth.models import User, Group
+from rest_framework.views import APIView
+
 
 @csrf_exempt
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
-class MenuItemsView(generics.ListCreateAPIView):
+@api_view(["GET", "POST"])
+class MenuItemsView(APIView):
     def get(self, request):
         queryset = MenuItem.objects.all()
         serializer_class = MenuItemSerializer(queryset, many=True)
@@ -81,3 +84,65 @@ class MenuItemsView(generics.ListCreateAPIView):
         except:
             return Response("Not authorized...", status=status.HTTP_403_FORBIDDEN)
 
+@csrf_exempt
+@throttle_classes([AnonRateThrottle, UserRateThrottle])
+@api_view(["GET", "PUT", "DELETE"])
+class MenuItemDetail(APIView):
+    def get_object(self, id):
+        try:
+            return MenuItem.objects.get(id=id)
+        except MenuItem.DoesNotExist:
+            pass
+
+    def get(self, request, id):
+        serializer_class = MenuItemSerializer(self.get_object(id))
+        return Response(serializer_class.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        try:
+            serializer_class = self.get_object(id)
+            data = MenuItemSerializer(serializer_class, data=request.data)
+            if request.user.groups.filter(name="Manager"):
+                if data.is_valid():
+                    data.save()
+                    return Response(data.data, status=status.HTTP_201_CREATED)
+        except:
+            return Response("Not authorised to make changes", status=status.HTTP_403_FORBIDDEN)
+
+    def delete(self, request, id):
+        try:
+            serializer_class = self.get_object(id)
+            if request.user.groups.filter(name="Manager"):
+                serializer_class.delete()
+                return Response("Deleted Successfully", status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response("Not authorised to remove item", status=status.HTTP_403_FORBIDDEN)
+
+@csrf_exempt
+@throttle_classes([AnonRateThrottle, UserRateThrottle])
+@api_view(["GET", "POST"])
+class UserGroupManagement(APIView):
+    def get(self, request):
+        try:
+            users = User.objects.filter(groups__name="Manager")
+            users_data = UserSerializer(users, many=True)
+            if request.user.groups.filter(name="Manager"):
+                return Response(users_data.data)
+        except:
+            return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
+
+    def post(self, request):
+        try:
+            if request.user.groups.filter(name="Manager"):
+                serializer_class = UserSerializer(data=request.data)
+                if serializer_class.is_valid():
+                    user = serializer_class.save()
+                    manager_group = Group.objects.get(name='Manager')
+                    manager_group.user_set.add(user)
+                    return Response(serializer_class.data, status=status.HTTP_201_CREATED)
+                return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response("Not authorized...", status=status.HTTP_401_UNAUTHORIZED)
+
+
+    
