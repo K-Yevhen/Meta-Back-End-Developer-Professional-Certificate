5cbc729b15e9aa9d3035287e4619362e9107ee7a
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-06-14 11:20:14 +0200

    testing

--- APIs/LittleLemon/LittleLemonDRF/urls.py
+++ APIs/LittleLemon/LittleLemonDRF/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import MenuItemsView, MenuItemDetail, UserGroupManagement, RemoveUserFromManagerGroup, DeliveryCrewManagerGroup, RemoveUserFromDeliveryCrewGroup, CartView, RemoveCartItem, OrderView, OrderDetail
-
+# from .views import MenuItemsView, MenuItemDetail, UserGroupManagement, RemoveUserFromManagerGroup, DeliveryCrewManagerGroup, RemoveUserFromDeliveryCrewGroup, CartView, RemoveCartItem, OrderView, OrderDetail
+from . import views
 # urlpatterns = [
 #     path('menu-items', views.MenuItemsView.as_view()),
 #     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
@@ -10,14 +10,14 @@ from .views import MenuItemsView, MenuItemDetail, UserGroupManagement, RemoveUse
 # ]
 #
 urlpatterns = [
-    path('menu-items/', views.MenuItemsView.as_view()),
-    path('menu-items/<int:id>', views.MenuItemDetail.as_view()),
-    path('groups/manager/users', UserGroupManagement.as_view()),
-    path('groups/manager/users/<int:id>', RemoveUserFromManagerGroup.as_view()),
-    path('groups/delivery-crew/users', DeliveryCrewManagerGroup.as_view()),
-    path('groups/delivery-crew/users/<int:id>', RemoveUserFromDeliveryCrewGroup.as_view()),
-    path('cart/menu-items', CartView.as_view()),
-    path('cart/menu-items/<int:id>', RemoveCartItem.as_view()),
-    path('orders/', OrderView.as_view()),
-    path('orders/<int:id>', OrderDetail.as_view()),
+    path('menu-items/', views.MenuItemsView),
+    path('menu-items/<int:id>', views.MenuItemDetail),
+    path('groups/manager/users', views.UserGroupManagement),
+    path('groups/manager/users/<int:id>', views.RemoveUserFromManagerGroup),
+    path('groups/delivery-crew/users', views.DeliveryCrewManagerGroup),
+    path('groups/delivery-crew/users/<int:id>', views.RemoveUserFromDeliveryCrewGroup),
+    path('cart/menu-items', views.CartView),
+    path('cart/menu-items/<int:id>', views.RemoveCartItem),
+    path('orders/', views.OrderView),
+    path('orders/<int:id>', views.OrderDetail),
 ]
--- APIs/LittleLemon/LittleLemonDRF/views.py
+++ APIs/LittleLemon/LittleLemonDRF/views.py
@@ -54,7 +54,6 @@ from django.contrib.auth.models import User, Group
 from rest_framework.views import APIView
 
 
-@csrf_exempt
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["GET", "POST"])
 class MenuItemsView(APIView):
@@ -65,26 +64,22 @@ class MenuItemsView(APIView):
         ordering_fields = ['title', 'price', 'category']
         filterset_fields = ['title', 'price', 'category']
         search_fields = ['category']
-        try:
-            if request.user.has_perm('LittleLemonAPI.view_menuitem'):
-                return Response(serializer_class.data, status=status.HTTP_200_OK)
-        except:
-            return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
+        if request.user.has_perm('LittleLemonAPI.view_menuitem'):
+            return Response(serializer_class.data, status=status.HTTP_200_OK)
+        return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
 
     def post(self, request):
-        try:
-            if request.user.groups.filter(name="Manager"):
-                data = MenuItemSerializer(data=request.data)
-                if data.is_valid():
-                    data.save()
-                    return Response(data.data, status=status.HTTP_201_CREATED)
-                else:
-                    return Response(data.errors, status=status.HTTP_403_FORBIDDEN)
-        except:
-            return Response("Not authorized...", status=status.HTTP_403_FORBIDDEN)
+        if request.user.groups.filter(name="Manager"):
+            data = MenuItemSerializer(data=request.data)
+            if data.is_valid():
+                data.save()
+                return Response(data.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(data.errors, status=status.HTTP_403_FORBIDDEN)
+        return Response("Not authorized...", status=status.HTTP_403_FORBIDDEN)
 
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["GET", "PUT", "DELETE"])
 class MenuItemDetail(APIView):
@@ -119,7 +114,7 @@ class MenuItemDetail(APIView):
             return Response("Not authorised to remove item", status=status.HTTP_403_FORBIDDEN)
 
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["GET", "POST"])
 class UserGroupManagement(APIView):
@@ -146,18 +141,16 @@ class UserGroupManagement(APIView):
             return Response("Not authorized...", status=status.HTTP_401_UNAUTHORIZED)
 
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["DELETE"])
 class RemoveUserFromManagerGroup(APIView):
     def delete(self, request, id):
-        # Getting a particular user object
         try:
             user = User.objects.get(id=id)
         except User.DoesNotExist:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        # Removing a user from the manager group by a manager
         group = Group.objects.get(name='Manager')
         if request.user.groups.filter(name="Manager"):
             if group in user.groups.all():
@@ -168,11 +161,10 @@ class RemoveUserFromManagerGroup(APIView):
         return Response("Not authorized", status=status.HTTP_401_UNAUTHORIZED)
 
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["GET", "POST"])
 class DeliveryCrewManagerGroup(APIView):
-    # Getting all delivery crew group members
     def get(self, request):
         users = User.objects.filter(groups__name="Delivery crew")
         serializer_class = UserSerializer(users, many=True)
@@ -180,7 +172,6 @@ class DeliveryCrewManagerGroup(APIView):
             return Response(serializer_class.data)
         return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
 
-    # Adding a user to delivery crew group through the payload
     def post(self, request, format=None):
         if request.user.groups.filter(name="Manager"):
             serializer_class = UserSerializer(data=request.data)
@@ -193,7 +184,7 @@ class DeliveryCrewManagerGroup(APIView):
         return Response("Not authorized...", status=status.HTTP_401_UNAUTHORIZED)
 
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["DELETE"])
 class RemoveUserFromDeliveryCrewGroup(APIView):
@@ -203,7 +194,6 @@ class RemoveUserFromDeliveryCrewGroup(APIView):
         except User.DoesNotExist:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        # Remove user from the deliveryCrewGroup
         group = Group.objects.get(name='Delivery crew')
         if request.user.groups.filter(name="Manager"):
             if group in user.groups.all():
@@ -214,11 +204,10 @@ class RemoveUserFromDeliveryCrewGroup(APIView):
         return Response("Not authorized", status=status.HTTP_401_UNAUTHORIZED)
 
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["GET", "POST"])
 class CartView(APIView):
-    # Getting all the cart items that belong to the signed user
     def get(self, request):
         user = request.user
         carts = Cart.objects.filter(user=user)
@@ -227,7 +216,6 @@ class CartView(APIView):
             return Response(serializer_class.data, status=status.HTTP_200_OK)
         return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
 
-    # Adding item to cart by a customer
     def post(self, request):
         if request.user.groups.filter(name="customer"):
             serializer_class = CartItemSerializer(data=request.data)
@@ -237,18 +225,16 @@ class CartView(APIView):
             return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response("Not authorized...", status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["DELETE"])
 class RemoveCartItem(APIView):
-    # Getting a particular cart item by the signed in user/customer
     def get_object(self, id):
         try:
             return Cart.objects.get(id=id)
         except Cart.DoesNotExist:
             return Response("Cart object not found", status=status.HTTP_404_NOT_FOUND)
 
-    # removing a cart item by the signed in user/customer
     def delete(self, request, id):
         if request.user.groups.filter(name="customer"):
             cart = self.get_object(id)
@@ -259,31 +245,25 @@ class RemoveCartItem(APIView):
         return Response("Not authorized to remove cart!", status=status.HTTP_401_UNAUTHORIZED)
 
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["GET", "POST"])
 class OrderView(APIView):
-    # Getting the order items by the authenticated user
     def get(self, request):
         user = request.user
         orders = Order.objects.filter(user=user)
         serializer_class = OrderSerializer(orders, many=True)
 
-    # order items by the authenticated user
         if request.user.groups.filter(name="customer"):
             return Response(serializer_class.data, status=status.HTTP_200_OK)
 
-    # all order items
         elif request.user.groups.filter(name="Manager"):
             all_orders = OrderItem.objects.all()
             all_orders_serializer = OrderItemSerializer(all_orders, many=True)
             return Response(all_orders_serializer.data, status=status.HTTP_200_OK)
 
-    # all order items assigned to a particular delivery crew
         elif request.user.groups.filter(name="Delivery crew"):
-            # Get all orders with order items assigned to the delivery crew
             orders = Order.objects.filter(delivery_crew=request.user)
-            # Serialize the orders and return them in a response object
             serializer_class = OrderSerializer(orders, many=True)
             return Response(serializer_class.data, status=status.HTTP_200_OK)
 
@@ -292,10 +272,8 @@ class OrderView(APIView):
     def post(self, request):
         if request.user.group.filter(name="customer"):
             user = request.user
-            # Get the cart items for the current user
             cart_items = Cart.objects.filter(user=user)
 
-            # Create a new order item for each cart item
             for item in cart_items:
                 order_item = OrderItem.objects.create(
                 order=request.user,
@@ -304,37 +282,27 @@ class OrderView(APIView):
                 unit_price=item.unit_price,
                 price=item.price,
             )
-
-            # Delete all the cart items for the current user
-            # client = Client()
-            # response = client.delete('/api/cart/menu-items')
             cart_items.delete()
             return Response({"message": "Order items created and cart items deleted successfully."}, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-@csrf_exempt
+
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
 @api_view(["GET", "PUT", "DELETE"])
 class OrderDetail(APIView):
-    # Getting a particular order item
     def get_object(self, id):
         try:
             return Order.objects.get(id=id)
         except Order.DoesNotExist:
             pass
-            # return Response('Order object not found', status=status.HTTP_404_NOT_FOUND)
 
-    # Particular order item detail by a customer
     def get(self, request, id):
         order_item = OrderSerializer(self.get_object(id))
-        # user = order_item.data.get('user').get('username')
-        # if request.user.username == user and request.user.groups.filter(name="customer"):
         if request.user.groups.filter(name="customer"):
             return Response(order_item.data, status=status.HTTP_200_OK)
         else:
             return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
 
-    # Update order item by a customer or manager
     def put(self, request, id):
         order_item = OrderSerializer(self.get_object(id))
         if request.user.groups.filter(name="customer" or "Manager"):
@@ -344,7 +312,6 @@ class OrderDetail(APIView):
                 return Response(serializer_class.data, status=status.HTTP_201_CREATED)
             return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        # Update the status of the particular order item when delivered
         elif request.user.groups.filter(name="Delivery crew"):
             order = OrderSerializer(order_item, data=request.data)
             if order.is_valid():
@@ -353,7 +320,6 @@ class OrderDetail(APIView):
             return Response(order.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response("Not authorized", status=status.HTTP_401_UNAUTHORIZED)
 
-    # Remove a particular order item by the manager
     def delete(self, request, id):
         if request.user.groups.filter(name="Manager"):
             order_item = self.get_object(id)
